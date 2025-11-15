ea91fd4eba2190641dbb96cea1bafc12a757cf8b
havyasu <havyasu2005@gmail.com>
2025-11-15 23:57:19 +0530

    update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, { useRef, useState } from 'react';
 import Button from "../Button/Button.jsx";
 
 const Hero = () => {
@@ -12,14 +12,14 @@ const Hero = () => {
     const nextVidRef = useRef(null);
 
     const handleVideoLoad = () => {
-        setLoadedVideos(prevState => prevState + 1)
-    }
+        setLoadedVideos(prevState => prevState + 1);
+    };
 
     const handleMiniVidClick = () => {
         setHasClicked(true);
         setCurrentIndex((pre) => {
             return (pre % totalVideos) + 1;
-        })
+        });
     };
 
     const getVideoSrc = (index) => {
@@ -35,16 +35,16 @@ const Hero = () => {
                             onClick={handleMiniVidClick}
                             className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100  hover:opacity-100  rounded-none  '>
                             <video ref={nextVidRef} src={getVideoSrc((currentIndex % totalVideos) + 1)} loop muted
-                                   id="current-video"
-                                   className='size-64 origin-center scale-150  object-cover  object-center  '
-                                   onLoadedData={handleVideoLoad}
+                                id="current-video"
+                                className='size-64 origin-center scale-150  object-cover  object-center  '
+                                onLoadedData={handleVideoLoad}
                             ></video>
                         </div>
                     </div>
                     <video ref={nextVidRef} src={getVideoSrc(currentIndex)} autoPlay loop muted id="next-video"
 
-                           className="absolute-center invisible absolute z-20 size-64 object-center object-cover  "
-                           onLoadedData={handleVideoLoad}
+                        className="absolute-center invisible absolute z-20 size-64 object-center object-cover  "
+                        onLoadedData={handleVideoLoad}
                     ></video>
 
                     <video
@@ -66,9 +66,9 @@ const Hero = () => {
                     <div className={"mt-24 px-5 sm:px-10"} >
                         <h1 className={"special-font hero-heading text-blue-100"} >REDEFI<b>N</b>E</h1>
                         <p className={"mb-5 max-w-64 font-robert-regular text-blue-100"} >
-                            Enter the Metagame Layer <br/>
+                            Enter the Metagame Layer <br />
                             Unleash the Play Economy
-                            <Button/>
+                            <Button />
                         </p>
                     </div>
 
